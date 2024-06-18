8ebf4c3b5bfb23e983c693dcf330ba5d2ead033e
Shivek-cmd <59970948+Shivek-cmd@users.noreply.github.com>
2024-06-18 21:00:31 +0530

    Initial Commit

---
+++ src/Components/News.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export default class News extends Component {
+  render() {
+    return (
+      <div className="container my-3">
+        <h2>NewsMonkey-Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="hola" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="hola" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="hola" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
